13e9aa0578d5256f5647afc621aa54bfdc049b2a
scdanya <gvozdevdan@gmail.com>
2024-12-27 15:33:54 +0300

    Autocommit from 2024-12-27 15:33:54
    Klipper version: v0.12.0-404-g80d185c94
    
    Moonraker version: v0.9.3-4-ga4604e3
    
    Mainsail version: v2.13.2

--- update_repo.sh
+++ update_repo.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-export LC_ALL=en_US.cp1251
-export LANG=en_US.cp1251
-
-git config --global core.quotepath off
-git config --global core.precomposeunicode true
-git config --global core.editor "vim -c ':set encoding=cp1251'"
-
 cd ~/printer_data/config
 
-git fetch
+git fetch 
 
 git reset origin/main
 git reset --hard HEAD
