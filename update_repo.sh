01810a47c00f51df5f635858f551c2b259ab4192
scdanya <gvozdevdan@gmail.com>
2024-12-27 14:41:30 +0300

    Autocommit from 2024-12-27 14:41:30
    Klipper version: v0.12.0-404-g80d185c94
    
    Moonraker version: v0.9.3-4-ga4604e3
    
    Mainsail version: v2.13.2

--- update_repo.sh
+++ update_repo.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+export LC_ALL=en_US.ANSI
+export LANG=en_US.ANSI
 
 git config --global core.quotepath off
 git config --global core.precomposeunicode true
-git config --global core.editor "vim -c ':set encoding=utf-8'"
+git config --global core.editor "vim -c ':set encoding=ansi'"
 
 cd ~/printer_data/config
 
